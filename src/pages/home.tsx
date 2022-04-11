bfdd789d7c693a27e7614b8a45b5de57943b85f2
Igor Santana <84198085+Igor-V-Santana@users.noreply.github.com>
2022-04-11 10:35:52 -0300

    Mostrando produtos da api na página home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,25 +1,28 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Cart from "../components/Cart";
 import { Container } from "../components/Container";
 import Header from "../components/Header";
-import Product, { ProductProps } from "../components/Product";
-
-const data: ProductProps = {
-  id: 1,
-  name: "Product 1",
-  picture:
-    "https://somos.lojaiplace.com.br/wp-content/uploads/2021/04/apple_iphone-12-spring21_purple_04202021.jpg",
-  price: 20.50,
-};
+import Product from "../components/Product";
 
 const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    const response = await axios.get("http://localhost:3001/products");
+    setProducts(response.data);
+  }
+
+  useEffect(() => {
+    getProducts();
+  }, [])
 
   return (
     <>
       <Header setIsOpen={setIsOpen} />
       <Container>
-        <Product {...data} />
+        {products.map(product => <Product {...product} />)}
         <Cart isOpen={isOpen} setIsOpen={setIsOpen} />
       </Container>
     </>
